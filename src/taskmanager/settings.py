b87cc65dde689b4db7f51db019cb5102ec76cc56
Alexandr Usenko <usenko.alexandr97@mail.ru>
2023-01-21 23:26:06 +0300

    change readme

--- src/taskmanager/settings.py
+++ src/taskmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =
+SECRET_KEY = 'django-insecure-wbcjgi@!m^-(41!t^sk-q%2dxchxkh%-1^4oo7ze8y4p*n@90k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
